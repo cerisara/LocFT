8136282776b276e573dc2eed2e9c52401f01adda
xtof <cerisara@loria.fr>
2026-05-16 17:46:29 +0200

    ok

--- courbes/gen.sh
+++ courbes/gen.sh
@@ -1,2 +1,2 @@
-echo 'plot "genbs8" u 3 w l t "BS8", "genbs16" u 3 w l t "BS16", "genbs32" u 3 w l t "BS32", "genbs64" u 3 w l t "BS64"' | gnuplot -p
+echo 'plot "genbs8" u 3 w l t "BS8", "genbs16" u 3 w l t "BS16", "genbs32" u 3 w l t "BS32", "genbs64" u 3 w l t "BS64", "genbs64r16" u 3 w l t "R16"' | gnuplot -p
 
