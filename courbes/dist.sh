9f15daae300f9d26ddeef1ccfb2a0019d5bcb470
xtof <cerisara@loria.fr>
2026-05-16 07:38:44 +0200

    ok

--- courbes/dist.sh
+++ courbes/dist.sh
@@ -1,2 +1,2 @@
-echo 'plot "distbs8" u 5 w l t "BS8", "distbs16" u 5 w l t "BS16", "distbs32" u 5 w l t "BS32"' | gnuplot -p
+echo 'plot "distbs8" u 5 w l t "BS8", "distbs16" u 5 w l t "BS16", "distbs32" u 5 w l t "BS32", "distbs64" u 5 w l t "BS64"' | gnuplot -p
 
