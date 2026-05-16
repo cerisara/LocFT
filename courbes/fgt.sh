4dc5bbb2dc3f8777cfaba4c3c156882235fc7ac8
xtofCC <xtof@ada.fr>
2026-05-16 07:36:16 +0200

    ok

--- courbes/fgt.sh
+++ courbes/fgt.sh
@@ -1,2 +1,2 @@
-echo 'plot "fgtbs8" u 3 w l t "BS8", "fgtbs16" u 3 w l t "BS16", "fgtbs32" u 3 w l t "BS32"' | gnuplot -p
+echo 'plot "fgtbs8" u 3 w l t "BS8", "fgtbs16" u 3 w l t "BS16", "fgtbs32" u 3 w l t "BS32", "fgtbs64" u 3 w l t "BS64"' | gnuplot -p
 
--- courbes/mem.sh
+++ courbes/mem.sh
@@ -1,2 +1,2 @@
-echo 'plot "membs8" u 3 w l t "BS8", "membs16" u 3 w l t "BS16", "membs32" u 3 w l t "BS32"' | gnuplot -p
+echo 'plot "membs8" u 3 w l t "BS8", "membs16" u 3 w l t "BS16", "membs32" u 3 w l t "BS32", "membs64" u 3 w l t "BS64"' | gnuplot -p
 
